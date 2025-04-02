bd203c174e7767a14ca3e3c5279f292d9c556617
RenTuruncu1 <turuncudesignerteam@gmail.com>
2025-04-02 13:17:34 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import FalciPaneli from "./FalciPaneli";
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <FalciPaneli />
+  </React.StrictMode>
+);
