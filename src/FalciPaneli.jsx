2f242643b3c1b79ef1ffd420908a6d4b5b76d320
RenTuruncu1 <turuncudesignerteam@gmail.com>
2025-04-02 13:33:04 +0300

    Delete src directory

--- src/FalciPaneli.jsx
+++
@@ -1,8 +0,0 @@
-export default function FalciPaneli() {
-  return (
-    <div style={{ padding: 40 }}>
-      <h1>Falcı Paneli</h1>
-      <p>Bu panel, falcıları listelemek, aramak ve yönetmek için tasarlanmıştır.</p>
-    </div>
-  );
-}
